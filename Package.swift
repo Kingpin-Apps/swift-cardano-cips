4a1e19bb5477ede2f05c8bcfa931f8bd315821f6
Hareem Adderley <hadderley@kingpinapps.com>
2026-05-29 07:24:06 -0500

    fix: update dependencies

--- Package.swift
+++ Package.swift
@@ -13,29 +13,25 @@ let package = Package(
       .visionOS(.v1),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftCardanoCIPs",
             targets: ["SwiftCardanoCIPs"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Kingpin-Apps/swift-cbor-codable.git", from: "0.3.0"),
-        .package(url: "https://github.com/Kingpin-Apps/swift-cose.git", from: "0.2.1"),
-        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.4.0"),
-        .package(url: "https://github.com/Kingpin-Apps/swift-ncal.git", .upToNextMinor(from: "0.3.0")),
-        // Provides CryptoKit-compatible APIs on Linux where CryptoKit itself is unavailable.
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.15.1"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "4.5.0"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cbor-codable.git", from: "0.3.2"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cose.git", from: "1.2.0"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-cardano-core.git", from: "0.4.3"),
+        .package(url: "https://github.com/Kingpin-Apps/swift-nacl.git", .upToNextMinor(from: "1.0.1")),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftCardanoCIPs",
             dependencies: [
                 .product(name: "CBORCodable", package: "swift-cbor-codable"),
                 .product(name: "SwiftCOSE", package: "swift-cose"),
                 .product(name: "SwiftCardanoCore", package: "swift-cardano-core"),
-                .product(name: "SwiftNcal", package: "swift-ncal"),
+                .product(name: "SwiftNaCl", package: "swift-nacl"),
                 .product(
                     name: "Crypto",
                     package: "swift-crypto",
